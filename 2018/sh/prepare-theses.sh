78f75186d82e2565794bd3662dc27e39f686a45d
nickkolok <nickkolok@mail.ru>
2017-12-16 15:27:09 +0300

    Пробел в конце не мешает end{document}

--- 2018/sh/prepare-theses.sh
+++ 2018/sh/prepare-theses.sh
@@ -5,8 +5,8 @@ cp $1 $1.bak
 iconv -f WINDOWS-1251 -t UTF-8 $1.bak |\
 grep -A 10000 -x '\\widowpenalty=10000' |\
 tail -n +2 |\
-grep -B 10000 -x '\\end{document}' |\
-head -n -1 >\
+grep  --invert-match -x '\\end{document}' |\
+grep  --invert-match -x '\\end{document} '>\
 $1
 
 #From: http://blog.e-shell.org/188
