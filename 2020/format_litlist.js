fa689d19cc346fcddb7ef297d372ac240274ef73
NickKolok <nickkolok@mail.ru>
2020-01-18 20:21:41 +0300

    A naive tool to format reference headers

---
+++ 2020/format_litlist.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const ls = require('ls');
+
+const texFiles = ls('Data/**.tex');
+
+
+const possibleLitListHeadings = [
+	'\\smallskip \\centerline {\\bf Литература} \\nopagebreak',
+	'\\centerline {\\bf Литература}',
+	'\\smallskip \\centerline{\\bf Литература}\\nopagebreak',
+	'\\smallskip \\centerline {\\bf Bibliography} \\nopagebreak',
+	'\\centerline {\\bf Bibliography}'
+];
+
+texFiles.map((file) => {
+	let rows = fs.readFileSync(file.full,'utf-8').split("\n");
+	let changed = false;
+
+	for(let i = 0; i < rows.length; i++){
+		let row = rows[i];
+		if (row.trim() == '\\litlist' || row.trim() == '\\reflist') {
+			// Список литературы в файле уже оформлен как надо
+			return;
+		}
+		for (let possibleRow of possibleLitListHeadings) {
+			//console.log(row.trim(), possibleRow)
+			if (row.trim() == possibleRow){
+				rows[i] = '\\litlist';
+				changed = true;
+				break;
+			}
+		}
+	}
+	if (changed) {
+		fs.writeFileSync(file.full,rows.join("\n"))
+	} else {
+		console.log(file.full + " : no litlist heading");
+	}
+})
