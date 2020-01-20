718bcd7e4cf34c8a87096fe4df738ef373906fd9
Sergey Baboshin <ijustbsd@gmail.com>
2020-01-20 03:56:54 +0300

    Closed #16

--- 2020/include_theses.sh
+++ 2020/include_theses.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for tex in Data/*.tex; do
+for tex in $(find Data -maxdepth 1 -name "[А-Яа-яёЁ]*.tex"); do
 	echo \\thesis{\\detokenize{$(basename "$tex")}};
 done | sort > theses.tex
+
+for tex in $(find Data -maxdepth 1 -name "[A-Za-z]*.tex"); do
+	echo \\thesis{\\detokenize{$(basename "$tex")}};
+done | sort >> theses.tex
